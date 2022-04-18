5e20b19a7dfce1583bd237a56268821235ceb45d
Ruslan Mashinistov <ruslan.mashinistov@desy.de>
2022-04-18 11:34:19 +0200

    New  API, retrieve GT by name

--- cdb_rest/urls.py
+++ cdb_rest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from cdb_rest.views import GlobalTagListCreationAPIView, GlobalTagDetailAPIView, GlobalTagStatusCreationAPIView, GlobalTagTypeCreationAPIView
-from cdb_rest.views import GlobalTagsListAPIView, GlobalTagsPayloadListsListAPIView
+from cdb_rest.views import GlobalTagsListAPIView, GlobalTagsPayloadListsListAPIView, GlobalTagByNameDetailAPIView
 from cdb_rest.views import PayloadListListCreationAPIView, PayloadTypeListCreationAPIView, PayloadIOVListCreationAPIView, PayloadListDetailAPIView
 from cdb_rest.views import PayloadIOVsListAPIView, PayloadIOVsList2APIView, PayloadIOVsRangesListAPIView, PayloadListDetailAPIView, PayloadIOVDetailAPIView
 from cdb_rest.views import PayloadListAttachAPIView, GlobalTagChangeStatusAPIView, PayloadIOVAttachAPIView
@@ -14,6 +14,7 @@ app_name = 'cdb_rest'
 urlpatterns = [
     path('gt', GlobalTagListCreationAPIView.as_view(), name="global_tag"),
     path('gt/<int:pk>', GlobalTagDetailAPIView.as_view(), name="global_tag_detail"),
+    path('globalTag/<str:globalTagName>', GlobalTagByNameDetailAPIView.as_view(), name="global_tag_detail"),
     path('gtstatus', GlobalTagStatusCreationAPIView.as_view(), name="global_tag_status"),
     path('gttype', GlobalTagTypeCreationAPIView.as_view(), name="global_tag_type"),
     path('globalTags', GlobalTagsListAPIView.as_view(), name="global_tags_list"),
