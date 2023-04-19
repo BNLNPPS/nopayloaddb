a169d76871bd98e9acb1f9eaf8d81f36905d9dcd
Ruslan Mashinistov <ruslan.mashinistov@desy.de>
2023-04-19 14:47:43 +0200

    Finalize ORM and raw SQL read APIs. Clean the old implementations

--- cdb_rest/queries.py
+++ cdb_rest/queries.py
@@ -1,36 +1,4 @@
 get_payload_iovs = '''
-WITH major_max_table AS(
-    SELECT m.payload_list_id, m.payload_url, m.major_iov, m.minor_iov, t.major_max
-    FROM (
-        SELECT payload_list_id, MAX(major_iov) AS major_max
-        FROM "PayloadIOV"
-        WHERE ((major_iov < %(my_major_iov)s) OR (major_iov = %(my_major_iov)s AND minor_iov <= %(my_minor_iov)s))
-        AND payload_list_id IN (
-            SELECT id FROM "PayloadList"
-            WHERE global_tag_id = (
-                SELECT id FROM "GlobalTag"
-                WHERE name = %(my_gt)s
-            )
-        )
-    GROUP BY payload_list_id
-    ) t JOIN "PayloadIOV" m ON m.payload_list_id = t.payload_list_id AND t.major_max = m.major_iov
-),
-major_minor_max_table AS(
-    SELECT n.payload_list_id, n.payload_url, n.major_iov, n.minor_iov
-    FROM (
-        SELECT payload_list_id, MAX(minor_iov) AS minor_max
-        FROM major_max_table
-        GROUP BY payload_list_id
-    ) u JOIN major_max_table n ON n.payload_list_id = u.payload_list_id AND u.minor_max = n.minor_iov
-)
-SELECT y.name AS payload_type_name, x.payload_url, x.major_iov, x.minor_iov
-FROM
-  major_minor_max_table x
-  JOIN "PayloadList" z ON x.payload_list_id = z.id
-  JOIN "PayloadType" y ON z.payload_type_id = y.id
-;
-'''
-get_payload_iovs2 = '''
 SELECT pt.name AS payload_type_name, pi.payload_url, pi.checksum, pi.major_iov, pi.minor_iov, pi.major_iov_end, pi.minor_iov_end
 FROM "PayloadList" pl
 JOIN "GlobalTag" gt ON pl.global_tag_id = gt.id AND gt.name = %(my_gt)s
