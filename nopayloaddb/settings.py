1c90e735424fe01311ea48a6f6ededcbaab6f5c1
ligerlac <lino.oscar.gerlach@cern.ch>
2023-07-28 17:41:24 +0200

    add optional test settings, remove unneeded folder

--- nopayloaddb/settings.py
+++ nopayloaddb/settings.py
@@ -90,6 +90,9 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': '{}/django-{}.log'.format(LOGPATH, HOSTNAME),
         },
+        'console': {
+            'class': 'logging.StreamHandler',
+        }
     },
     'loggers': {
         'django': {
