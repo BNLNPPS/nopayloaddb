b336c944fd2a7a740980f80c8aad4322f1b60924
ruslan33 <rmashinistov@gmail.com>
2025-01-02 13:15:16 +0100

    Split read-only and read/write API by the db backends (#34)

---
+++ nopayloaddb/db_router.py
@@ -0,0 +1,29 @@
+import random
+from nopayloaddb.middleware import get_current_request
+
+logger = logging.getLogger(__name__)
+
+class ReadWriteRouter:
+    def db_for_read(self, model, **hints):
+        """Route read queries to one of the read databases."""
+        db = random.choice(['read_db_1', 'read_db_2'])
+        # Check if request information is available
+        request = get_current_request()
+        if request and request.method == 'GET':
+            return db
+        return 'default'
+
+    def db_for_write(self, model, **hints):
+        """Route write queries to the default database."""
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """Allow any relation if both objects are in the same database."""
+        db_set = {'default', 'read_db_1', 'read_db_2'}
+        if obj1._state.db in db_set and obj2._state.db in db_set:
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """Ensure migrations only run on the default database."""
+        return db == 'default'
