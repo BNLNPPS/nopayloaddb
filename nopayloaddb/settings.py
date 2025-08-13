1731e51c1edf4c3b7b9e315b4edeed58e06d898c
ruslan33 <rmashinistov@gmail.com>
2025-08-13 17:10:30 +0200

    DB CONN_MAX_AGE = 10 s (#54)
    * Set DB CONN_MAX_AGE to 10 sec

--- nopayloaddb/settings.py
+++ nopayloaddb/settings.py
@@ -148,7 +148,8 @@ DATABASES = {
         'HOST':     os.environ.get("POSTGRES_HOST_W",     default='localhost'),
         'PORT':     os.environ.get("POSTGRES_PORT_W",     default='5432'),
 
-        'CONN_MAX_AGE': 60,  # Close and reopen every 1m
+        'CONN_MAX_AGE': 10,  # Close and reopen every 10s
+
     },
     'read_db_1': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -160,7 +161,8 @@ DATABASES = {
         'HOST':     os.environ.get("POSTGRES_HOST_R1",     default='localhost'),
         'PORT':     os.environ.get("POSTGRES_PORT_R1",     default='5432'),
 
-        'CONN_MAX_AGE': 60,  # Close and reopen every 1m
+        'CONN_MAX_AGE': 10,  # Close and reopen every 10s
+
     },
     'read_db_2': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -172,7 +174,8 @@ DATABASES = {
         'HOST':     os.environ.get("POSTGRES_HOST_R2",     default='localhost'),
         'PORT':     os.environ.get("POSTGRES_PORT_R2",     default='5432'),
 
-        'CONN_MAX_AGE': 60,  # Close and reopen every 1m
+        'CONN_MAX_AGE': 10,  # Close and reopen every 10s
+
     },
 }
 
