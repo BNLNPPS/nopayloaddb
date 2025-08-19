b4862ab62383499a285f18bb7a76250423fbd38a
Ruslan <rmashinistov@gmail.com>
2025-08-19 21:22:52 +0200

    Add functional permissions

--- cdb_rest/permissions_plugins/belle2.py
+++ cdb_rest/permissions_plugins/belle2.py
@@ -1,7 +1,17 @@
 from .base import BasePermissionPlugin
+import re
 
 class Belle2PermissionPlugin(BasePermissionPlugin):
     def has_permission(self, request, context: dict) -> bool:
         claims = getattr(request.user, "claims", {})
-        allowed_objects = claims.get("allowed_objects", [])
-        return target_object in allowed_objects
+        #allowed_objects = claims.get("allowed_objects", [])
+        if context.get("object") in ("GlobalTag"):
+            name = context.get("name", "")
+            patterns = claims.get('b2cdb:admin', [])
+
+            match_found = any(re.fullmatch(pattern, name) for pattern in patterns)
+
+            if match_found:
+                return True
+                   
+            return False
