5ce1cc8c29d4259ee0cda333fbfa56871b80741f
Ruslan <rmashinistov@gmail.com>
2025-08-21 16:19:46 +0200

    Add permission check to the views

--- cdb_rest/permissions_plugins/belle2.py
+++ cdb_rest/permissions_plugins/belle2.py
@@ -3,15 +3,30 @@ import re
 
 class Belle2PermissionPlugin(BasePermissionPlugin):
     def has_permission(self, request, context: dict) -> bool:
+        match_found = False
         claims = getattr(request.user, "claims", {})
         #allowed_objects = claims.get("allowed_objects", [])
         if context.get("object") in ("GlobalTag"):
             name = context.get("name", "")
-            patterns = claims.get('b2cdb:admin', [])
-
+            role = context.get("role", "")
+            patterns = claims.get('b2cdb:admin', []) #createpayload, createiov
             match_found = any(re.fullmatch(pattern, name) for pattern in patterns)
+            if role == "createiov":
+                patterns = claims.get('b2cdb:createiov', [])
+                match_found = match_found or any(re.fullmatch(pattern, name) for pattern in claims.get('b2cdb:createiov', []))
 
             if match_found:
                 return True
+            
+        if context.get("object") in ("PayloadList"):
+            name = context.get("name", "")
+            role = context.get("role", "")
+            if role == "createpayload":
+                patterns = claims.get('b2cdb:createpayload', [])
+                match_found = any(re.fullmatch(pattern, name) for pattern in patterns)
+
+                if match_found:
+                    return True
+            
                    
             return False
